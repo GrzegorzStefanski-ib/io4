46b03f297eb4ee8425fbfb984cfd647d541b4bcd
Grzegorz Stefański <stefanski.grzegorz6@gmail.com>
2021-04-12 15:12:29 +0200

    1.1.2

---
+++ src/main/java/pl/io/springdata/Start.java
@@ -0,0 +1,23 @@
+package pl.io.springdata;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Start {
+
+    private ProductRepo productRepo;
+
+    @Autowired
+    public Start(ProductRepo productRepo) {
+        this.productRepo = productRepo;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void run() {
+        Product product = new Product("Ołówek", (float) 1.99, true);
+        productRepo.save(product);
+    }
+}
