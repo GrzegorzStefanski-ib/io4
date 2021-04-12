61c523427b57abce50d7500209879a1fb3bdb529
Grzegorz Stefański <stefanski.grzegorz6@gmail.com>
2021-04-12 13:55:57 +0200

    init

---
+++ src/main/java/pl/io/springdata/SpringdataApplication.java
@@ -0,0 +1,13 @@
+package pl.io.springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdataApplication.class, args);
+    }
+
+}
