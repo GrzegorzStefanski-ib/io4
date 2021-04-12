ef0781cbe11009919138d70c16d7faa806e7c65e
Grzegorz Stefański <stefanski.grzegorz6@gmail.com>
2021-04-12 18:34:53 +0200

    1.1.5

---
+++ src/main/java/pl/io/springdata/CustomerRepo.java
@@ -0,0 +1,8 @@
+package pl.io.springdata;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepo extends CrudRepository<Customer, Long> {
+}
--- src/main/java/pl/io/springdata/Start.java
+++ src/main/java/pl/io/springdata/Start.java
@@ -1,32 +1,30 @@
-package pl.io.springdata;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
-
-import java.math.BigDecimal;
-
-@Component
-public class Start {
-
-    private ProductRepo productRepo;
-
-    @Autowired
-    public Start(ProductRepo productRepo) {
-        this.productRepo = productRepo;
-    }
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void run() {
-        Product product = new Product("Ołówek",  1.99f, true);
-        productRepo.save(product);
-
-        product = new Product("Blok", 8.59f, true);
-        productRepo.save(product);
-
-        product = new Product("Gumka", 2.22f, false);
-        productRepo.save(product);
-    }
-
-}
+//package pl.io.springdata;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.context.event.ApplicationReadyEvent;
+//import org.springframework.context.event.EventListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class Start {
+//
+//    private ProductRepo productRepo;
+//
+//    @Autowired
+//    public Start(ProductRepo productRepo) {
+//        this.productRepo = productRepo;
+//    }
+//
+//    @EventListener(ApplicationReadyEvent.class)
+//    public void run() {
+//        Product product = new Product("Ołówek",  1.99f, true);
+//        productRepo.save(product);
+//
+//        product = new Product("Blok", 8.59f, true);
+//        productRepo.save(product);
+//
+//        product = new Product("Gumka", 2.22f, false);
+//        productRepo.save(product);
+//    }
+//
+//}
